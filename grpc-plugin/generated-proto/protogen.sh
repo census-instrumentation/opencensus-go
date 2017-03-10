4a8feaa47f5ce80d8e258280da785d4100bfc502
acetechnologist <acetechnologist@gmail.com>
2017-03-09 23:27:11 -0800

    Added sample clietn/server proto file

--- grpc-plugin/generated-proto/protogen.sh
+++ grpc-plugin/generated-proto/protogen.sh
@@ -10,8 +10,8 @@ git clone https://github.com/grpc/grpc-proto.git
 go_source="GOPATH/src/"
 output_directory=$go_source"github.com/google/instrumentation-go/grpc-plugin/generated-proto"
 input_directory=$go_source"github.com/google/instrumentation-proto"
-~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=$output_directory --proto_path="${input_directory}" $input_directory/stats/*.proto
-~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=$output_directory --proto_path="${input_directory}" $input_directory/service/monitoring.proto
+~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=plugins=grpc:$output_directory --proto_path="${input_directory}" $input_directory/stats/*.proto
+~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=plugins=grpc:$output_directory --proto_path="${input_directory}" $input_directory/service/monitoring.proto
 
 
 
@@ -20,3 +20,23 @@ input_directory=$go_source"github.com/google/instrumentation-proto"
 // go build ./grpc-plugin/collection-plugin/
 // go test -v -bench=. ./instrumentation-go-grpc/adapter/
 // go test -v -bench=. ./stats/
+
+
+
+
+// http://www.grpc.io/docs/quickstart/go.html
+// go get google.golang.org/grpc
+// download released protoc from https://github.com/google/protobuf/releases --> https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-osx-x86_64.zip
+// add it to PATH (~/.profile; source profile OR /etc/environment; source /etc/environment)
+// go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+// export PATH=$PATH:$GOPATH/bin
+// protoc --go_out=plugins=grpc:. *.proto
+// go get github.com/google/instrumentation-proto
+// go get github.com/grpc/grpc-proto
+
+
+go_source="GOPATH/src/"
+output_directory=$go_source"github.com/google/instrumentation-go/grpc-plugin/generated-proto"
+input_directory=$go_source"github.com/google/instrumentation-proto"
+~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=$output_directory --proto_path="${input_directory}" $input_directory/stats/*.proto
+~/Documents/protoc-3.2.0-linux-x86_64/bin/protoc --go_out=$output_directory --proto_path="${input_directory}" $input_directory/service/monitoring.proto
