a39e2d4367f8c624c84993d0368def71b7befeab
JBD <jbd@golang.org>
2017-11-19 13:28:38 -0800

    Merge related types into the same files
    To reduce context switch while reading and writing, merge
    closely related types into the same files.

--- stats/collector.go
+++ stats/collector.go
@@ -16,8 +16,10 @@
 package stats
 
 import (
+	"sort"
 	"time"
 
+	"go.opencensus.io/internal/tagencoding"
 	"go.opencensus.io/tag"
 )
 
@@ -54,4 +56,33 @@ func (c *collector) collectedRows(keys []tag.Key, now time.Time) []*Row {
 
 func (c *collector) clearRows() {
 	c.signatures = make(map[string]aggregator)
+}
+
+// encodeWithKeys encodes the map by using values
+// only associated with the keys provided.
+func encodeWithKeys(m *tag.Map, keys []tag.Key) []byte {
+	vb := &tagencoding.Values{
+		Buffer: make([]byte, len(keys)),
+	}
+	for _, k := range keys {
+		v, _ := m.Value(k)
+		vb.WriteValue([]byte(v))
+	}
+	return vb.Bytes()
+}
+
+// decodeTags decodes tags from the buffer and
+// orders them by the keys.
+func decodeTags(buf []byte, keys []tag.Key) []tag.Tag {
+	vb := &tagencoding.Values{Buffer: buf}
+	var tags []tag.Tag
+	for _, k := range keys {
+		v := vb.ReadValue()
+		if v != nil {
+			tags = append(tags, tag.Tag{Key: k, Value: string(v)})
+		}
+	}
+	vb.ReadIndex = 0
+	sort.Slice(tags, func(i, j int) bool { return tags[i].Key.Name() < tags[j].Key.Name() })
+	return tags
 }
