289294fcb3893dd73b87f11f2b8acc1475fc1cca
JBD <burcujdogan@gmail.com>
2017-11-27 10:02:05 -0800

    Add a hello world gRPC example (#149)
    Add a gRPC client and server using the OpenCensus stats handler
    to trace and collect stats.

---
+++ examples/grpc/helloworld_client/main.go
@@ -0,0 +1,70 @@
+// Copyright 2017, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"go.opencensus.io/examples/grpc/exporter"
+	pb "go.opencensus.io/examples/grpc/proto"
+	ocgrpc "go.opencensus.io/plugins/grpc"
+	"go.opencensus.io/plugins/grpc/grpcstats"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/trace"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Register stats and trace exporters to export
+	// the collected data.
+	stats.RegisterExporter(&exporter.StatsLogExporter{})
+	trace.RegisterExporter(&exporter.TraceLogExporter{})
+
+	// Subscribe to collect client request count.
+	if err := grpcstats.RPCClientRequestCountView.Subscribe(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Set up a connection to the server with the OpenCensus
+	// stats handler to enable stats and tracing.
+	conn, err := grpc.Dial(address, grpc.WithStatsHandler(ocgrpc.NewClientStatsHandler()), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+
+	stats.SetReportingPeriod(time.Second)
+	time.Sleep(2 * time.Second) // Wait for the data collection.
+}
