c5e712e5a7e283aa08017f727210068ed421f97d
Jonathan Amsterdam <jba@users.noreply.github.com>
2017-12-07 17:22:38 -0500

    trace: improve doc (#229)
    * trace: improve doc
    
    Add a package-level comment that has a couple of standard use cases.
    
    Add an example.
    
    * reviewer-suggested changes

--- trace/basetypes.go
+++ trace/basetypes.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package trace contains types for representing trace information, and
-// functions for global configuration of tracing.
 package trace
 
 import (
