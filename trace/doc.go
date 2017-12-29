4e34dfc2e21f0fb4a6b449cee4354729f8384c0e
Emmanuel Odeke <emm.odeke@gmail.com>
2017-12-29 00:38:56 -0700

    Tweak wording of StartSpan for an already existent span
    The previous doc seemed to imply that a child span would
    be created if "the child span" already existed but
    we meant to say that the child span would only be created
    if there was an already existent span with the same name.

--- trace/doc.go
+++ trace/doc.go
@@ -46,8 +46,8 @@ lines to the top of the function:
     ctx = trace.StartSpan(ctx, "your choice of name")
     defer trace.EndSpan(ctx)
 
-StartSpan will create a child span if one already exists, and will create a
-new top-level span otherwise.
+Invoking StartSpan with the name of an already existent span will create a child span,
+otherwise it will create a new top-level span.
 
 As a suggestion, use the fully-qualified function name as the span name, e.g.
 "github.com/me/mypackage.Run".
