fd114d95cf0f9fd0fd6879ce84b7be8479074701
Ramon Nogueira <rmn@google.com>
2018-02-05 14:02:28 -0800

    Add generated Go files.

---
+++ proto/traceproto/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2017, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Packaage opencensus_proto_trace contains Protobuf generated code for recording traces.
+package opencensus_proto_trace // import "go.opencensus.io/proto/traceproto"
+
+//go:generate protoc --go_out=./ -I../opencensus-proto/opencensus/proto/trace ../opencensus-proto/opencensus/proto/trace/trace.proto
+
