b90813af5822ba0bd06c365bf718e52dfe4ac724
Ramon Nogueira <rmn@google.com>
2018-04-14 20:46:44 +0200

    Add back ClientRequestCountView, it is used by cloud.google.com/go

--- plugin/ocgrpc/benchmark_test.go
+++ plugin/ocgrpc/benchmark_test.go
@@ -1,3 +1,17 @@
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ocgrpc
 
 import (
@@ -46,7 +60,6 @@ var codeToStringMap = map[codes.Code]string{
 func BenchmarkMapAlternativeImpl_OK(b *testing.B) {
 	st := status.New(codes.OK, "OK")
 	for i := 0; i < b.N; i++ {
-		s := codeToStringMap[st.Code()]
-		_ = s
+		_ = codeToStringMap[st.Code()]
 	}
 }
