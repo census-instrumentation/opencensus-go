4356161e0521189ca4509340c8bd6a461ed3b2c1
Emmanuel T Odeke <emmanuel@orijtech.com>
2018-05-09 23:42:44 -0700

    Make plugin/ochttp/Handler implement common HTTP server interfaces
    For #744
    
    Make handler implement the following interfaces:
    * http.CloseNotifier
    * http.Pusher
    and also adds tests to lock this behavior in and ensure
    that we never regress.
    
    In the future, we'll need to figure out why net/http is
    still tripping out with http.ErrNotSupported for any
    sort of server/connection combination with or without
    ochttp.Handler as the handler. For now the current
    code I believe satisfactorily implements the http.Pusher
    interface.

--- plugin/ochttp/server_test.go
+++ plugin/ochttp/server_test.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"sync"
 	"testing"
+	"time"
 
 	"golang.org/x/net/http2"
 
@@ -350,4 +351,202 @@ var _ http.RoundTripper = (*testResponseTransport)(nil)
 
 func (rb *testResponseTransport) RoundTrip(*http.Request) (*http.Response, error) {
 	return rb.res, nil
+}
+
+func TestHandlerImplementsHTTPPusher(t *testing.T) {
+	cst := setupAndStartServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		pusher, ok := w.(http.Pusher)
+		if !ok {
+			w.Write([]byte("false"))
+			return
+		}
+		err := pusher.Push("/static.css", &http.PushOptions{
+			Method: "GET",
+			Header: http.Header{"Accept-Encoding": r.Header["Accept-Encoding"]},
+		})
+		if err != nil && false {
+			// TODO: (@odeke-em) consult with Go stdlib for why trying
+			// to configure even an HTTP/2 server and HTTP/2 transport
+			// still return http.ErrNotSupported even without using ochttp.Handler.
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Write([]byte("true"))
+	}), asHTTP2)
+	defer cst.Close()
+
+	tests := []struct {
+		rt       http.RoundTripper
+		wantBody string
+	}{
+		{
+			rt:       h1Transport(),
+			wantBody: "true",
+		},
+		{
+			rt:       h2Transport(),
+			wantBody: "true",
+		},
+		{
+			rt:       &Transport{Base: h1Transport()},
+			wantBody: "true",
+		},
+		{
+			rt:       &Transport{Base: h2Transport()},
+			wantBody: "true",
+		},
+	}
+
+	for i, tt := range tests {
+		c := &http.Client{Transport: &Transport{Base: tt.rt}}
+		res, err := c.Get(cst.URL)
+		if err != nil {
+			t.Errorf("#%d: Unexpected error %v", i, err)
+			continue
+		}
+		body, _ := ioutil.ReadAll(res.Body)
+		_ = res.Body.Close()
+		if g, w := string(body), tt.wantBody; g != w {
+			t.Errorf("#%d: got = %q; want = %q", i, g, w)
+		}
+	}
+}
+
+const (
+	isNil       = "isNil"
+	hang        = "hang"
+	ended       = "ended"
+	nonNotifier = "nonNotifier"
+
+	asHTTP1 = false
+	asHTTP2 = true
+)
+
+func setupAndStartServer(hf func(http.ResponseWriter, *http.Request), isHTTP2 bool) *httptest.Server {
+	cst := httptest.NewUnstartedServer(&Handler{
+		Handler: http.HandlerFunc(hf),
+	})
+	if isHTTP2 {
+		http2.ConfigureServer(cst.Config, new(http2.Server))
+		cst.TLS = cst.Config.TLSConfig
+		cst.StartTLS()
+	} else {
+		cst.Start()
+	}
+
+	return cst
+}
+
+func insecureTLS() *tls.Config     { return &tls.Config{InsecureSkipVerify: true} }
+func h1Transport() *http.Transport { return &http.Transport{TLSClientConfig: insecureTLS()} }
+func h2Transport() *http.Transport {
+	tr := &http.Transport{TLSClientConfig: insecureTLS()}
+	http2.ConfigureTransport(tr)
+	return tr
+}
+
+type concurrentBuffer struct {
+	sync.RWMutex
+	bw *bytes.Buffer
+}
+
+func (cw *concurrentBuffer) Write(b []byte) (int, error) {
+	cw.Lock()
+	defer cw.Unlock()
+
+	return cw.bw.Write(b)
+}
+
+func (cw *concurrentBuffer) String() string {
+	cw.Lock()
+	defer cw.Unlock()
+
+	return cw.bw.String()
+}
+
+func handleCloseNotify(outLog io.Writer) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cn, ok := w.(http.CloseNotifier)
+		if !ok {
+			fmt.Fprintln(outLog, nonNotifier)
+			return
+		}
+		ch := cn.CloseNotify()
+		if ch == nil {
+			fmt.Fprintln(outLog, isNil)
+			return
+		}
+
+		<-ch
+		fmt.Fprintln(outLog, ended)
+	})
+}
+
+func TestHandlerImplementsHTTPCloseNotify(t *testing.T) {
+	http1Log := &concurrentBuffer{bw: new(bytes.Buffer)}
+	http1Server := setupAndStartServer(handleCloseNotify(http1Log), asHTTP1)
+	http2Log := &concurrentBuffer{bw: new(bytes.Buffer)}
+	http2Server := setupAndStartServer(handleCloseNotify(http2Log), asHTTP2)
+
+	defer http1Server.Close()
+	defer http2Server.Close()
+
+	tests := []struct {
+		url  string
+		want string
+	}{
+		{url: http1Server.URL, want: nonNotifier},
+		{url: http2Server.URL, want: ended},
+	}
+
+	transports := []struct {
+		name string
+		rt   http.RoundTripper
+	}{
+		{name: "http2+ochttp", rt: &Transport{Base: h2Transport()}},
+		{name: "http1+ochttp", rt: &Transport{Base: h1Transport()}},
+		{name: "http1-ochttp", rt: h1Transport()},
+		{name: "http2-ochttp", rt: h2Transport()},
+	}
+
+	// Each transport invokes one of two server types, either HTTP/1 or HTTP/2
+	for _, trc := range transports {
+		// Try out all the transport combinations
+		for i, tt := range tests {
+			req, err := http.NewRequest("GET", tt.url, nil)
+			if err != nil {
+				t.Errorf("#%d: Unexpected error making request: %v", i, err)
+				continue
+			}
+
+			// Using a timeout to ensure that the request is cancelled and the server
+			// if its handler implements CloseNotify will see this as the client leaving.
+			ctx, cancel := context.WithTimeout(context.Background(), 80*time.Millisecond)
+			defer cancel()
+			req = req.WithContext(ctx)
+
+			client := &http.Client{Transport: trc.rt}
+			res, err := client.Do(req)
+			if err != nil && !strings.Contains(err.Error(), "context deadline exceeded") {
+				t.Errorf("#%d: %sClient Unexpected error %v", i, trc.name, err)
+				continue
+			}
+			if res != nil && res.Body != nil {
+				io.CopyN(ioutil.Discard, res.Body, 5)
+				_ = res.Body.Close()
+			}
+		}
+	}
+
+	// Wait for a couple of milliseconds for the GoAway frames to be properly propagated
+	<-time.After(150 * time.Millisecond)
+
+	wantHTTP1Log := strings.Repeat("ended\n", len(transports))
+	wantHTTP2Log := strings.Repeat("ended\n", len(transports))
+	if g, w := http1Log.String(), wantHTTP1Log; g != w {
+		t.Errorf("HTTP1Log got\n\t%q\nwant\n\t%q", g, w)
+	}
+	if g, w := http2Log.String(), wantHTTP2Log; g != w {
+		t.Errorf("HTTP2Log got\n\t%q\nwant\n\t%q", g, w)
+	}
 }
