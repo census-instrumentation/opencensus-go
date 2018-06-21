census-instrumentation/opencensus-go

commit 6edeb78af2d9e4f169abb223feaef35da2e45d06

Initial support for lazily enabling zpages at runtime (#774)
We still need to actually remove zpage.Handler before we can remove the eager initialization. Will will do this in around a month according to the deprecation policy.

See: #772

zpages/zpages.go
@@ -32,15 +32,39 @@ package zpages // import "go.opencensus.io/zpages"
 
 import (
 	"net/http"
+	"path"
+	"sync"
+
+	"go.opencensus.io/internal"
 )
 
-// Handler is an http.Handler that serves the zpages.
+// TODO(ramonza): Remove Handler to make initialization lazy.
+
+// Handler is deprecated: Use Handle.
 var Handler http.Handler
 
 func init() {
-	zpagesMux := http.NewServeMux()
-	zpagesMux.HandleFunc("/rpcz", rpczHandler)
-	zpagesMux.HandleFunc("/tracez", tracezHandler)
-	zpagesMux.Handle("/public/", http.FileServer(fs))
-	Handler = zpagesMux
+	mux := http.NewServeMux()
+	Handle(mux, "")
+	Handler = mux
+}
+
+// Handle adds the z-pages to the given ServeMux rooted at pathPrefix.
+func Handle(mux *http.ServeMux, pathPrefix string) {
+	enable()
+	if mux == nil {
+		mux = http.DefaultServeMux
+	}
+	mux.HandleFunc(path.Join(pathPrefix, "rpcz"), rpczHandler)
+	mux.HandleFunc(path.Join(pathPrefix, "tracez"), tracezHandler)
+	mux.Handle(path.Join(pathPrefix, "public/"), http.FileServer(fs))
+}
+
+var enableOnce sync.Once
+
+func enable() {
+	enableOnce.Do(func() {
+		internal.LocalSpanStoreEnabled = true
+		registerRPCViews()
+	})
 }

zpages/zpages_test.go
@@ -21,6 +21,10 @@ import (
 	"testing"
 	"time"
 
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
 	"go.opencensus.io/trace"
 )
 
@@ -83,5 +87,25 @@ func TestTraceRows(t *testing.T) {
 2006/01/02-15:04:05.123456      .500000 sent message [200 bytes, 100 compressed bytes]
 `; buf.String() != want {
 		t.Errorf("writeTextTraces: got %q want %q\n", buf.String(), want)
+	}
+}
+
+func TestGetZPages(t *testing.T) {
+	mux := http.NewServeMux()
+	Handle(mux, "/debug")
+	server := httptest.NewServer(mux)
+	defer server.Close()
+	tests := []string{"/debug/rpcz", "/debug/tracez"}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("GET %s", tt), func(t *testing.T) {
+			res, err := http.Get(server.URL + tt)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if got, want := res.StatusCode, http.StatusOK; got != want {
+				t.Errorf("res.StatusCode = %d; want %d", got, want)
+			}
+		})
 	}
 }

examples/grpc/helloworld_server/main.go
@@ -47,10 +47,13 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func main() {
+	// Start z-Pages server.
 	go func() {
-		http.Handle("/debug/", http.StripPrefix("/debug", zpages.Handler))
-		log.Fatal(http.ListenAndServe(":8081", nil))
+		mux := http.NewServeMux()
+		zpages.Handle(mux, "/debug")
+		log.Fatal(http.ListenAndServe("127.0.0.1:8081", mux))
 	}()
+
 	// Register stats and trace exporters to export
 	// the collected data.
 	view.RegisterExporter(&exporter.PrintExporter{})

examples/http/helloworld_server/main.go
@@ -29,7 +29,12 @@ import (
 )
 
 func main() {
-	go func() { log.Fatal(http.ListenAndServe(":8081", zpages.Handler)) }()
+	// Start z-Pages server.
+	go func() {
+		mux := http.NewServeMux()
+		zpages.Handle(mux, "/debug")
+		log.Fatal(http.ListenAndServe("127.0.0.1:8081", mux))
+	}()
 
 	// Register stats and trace exporters to export the collected data.
 	exporter := &exporter.PrintExporter{}

zpages/example_test.go
@@ -22,7 +22,7 @@ import (
 )
 
 func Example() {
-	// Both /debug/tracez and /debug/rpcz will be served.
-	http.Handle("/debug/", http.StripPrefix("/debug", zpages.Handler))
-	log.Fatal(http.ListenAndServe(":9999", nil))
+	// Both /debug/tracez and /debug/rpcz will be served on the default mux.
+	zpages.Handle(nil, "/debug")
+	log.Fatal(http.ListenAndServe("127.0.0.1:9999", nil))
 }

zpages/rpcz.go
@@ -56,7 +56,7 @@ var (
 	}
 )
 
-func init() {
+func registerRPCViews() {
 	views := make([]*view.View, 0, len(viewType))
 	for v := range viewType {
 		views = append(views, v)

zpages/tracez.go
@@ -75,10 +75,6 @@ var (
 	}
 )
 
-func init() {
-	internal.LocalSpanStoreEnabled = true
-}
-
 func canonicalCodeString(code int32) string {
 	if code < 0 || int(code) >= len(canonicalCodes) {
 		return "error code " + strconv.FormatInt(int64(code), 10)

commit 3ce759a81bb459eb655205c08b4a0a44e16d3951

Fix zpage.Handler
The deprecated zpage.Handler was broken by 6edeb78a.

zpages/zpages.go
@@ -45,7 +45,7 @@ var Handler http.Handler
 
 func init() {
 	mux := http.NewServeMux()
-	Handle(mux, "")
+	Handle(mux, "/")
 	Handler = mux
 }
 

zpages/zpages_test.go
@@ -109,3 +109,21 @@ func TestGetZPages(t *testing.T) {
 		})
 	}
 }
+
+func TestGetZPages_default(t *testing.T) {
+	server := httptest.NewServer(Handler)
+	defer server.Close()
+	tests := []string{"/rpcz", "/tracez"}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("GET %s", tt), func(t *testing.T) {
+			res, err := http.Get(server.URL + tt)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if got, want := res.StatusCode, http.StatusOK; got != want {
+				t.Errorf("res.StatusCode = %d; want %d", got, want)
+			}
+		})
+	}
+}