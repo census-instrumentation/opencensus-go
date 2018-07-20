2b984bd18b2e315de639909f8a9f35140c0e118b
Bas van Beek <basvanbeek@users.noreply.github.com>
2018-07-21 03:20:20 +0800

    optimizes Span.End() to return faster if possible (#832)
    Span.End() optimizations:
    
    * only create spanData if needed for spanStore or export (sampled span with at least one registered exporter)
    * lock free exporter iteration
    * made gRPC test traceExporter safe for concurrent use

--- trace/trace.go
+++ trace/trace.go
@@ -248,19 +248,19 @@ func (s *Span) End() {
 		if s.executionTracerTaskEnd != nil {
 			s.executionTracerTaskEnd()
 		}
-		// TODO: optimize to avoid this call if sd won't be used.
-		sd := s.makeSpanData()
-		sd.EndTime = internal.MonotonicEndTime(sd.StartTime)
-		if s.spanStore != nil {
-			s.spanStore.finished(s, sd)
-		}
-		if s.spanContext.IsSampled() {
-			// TODO: consider holding exportersMu for less time.
-			exportersMu.Lock()
-			for e := range exporters {
-				e.ExportSpan(sd)
+		exp, _ := exporters.Load().(exportersMap)
+		mustExport := s.spanContext.IsSampled() && len(exp) > 0
+		if s.spanStore != nil || mustExport {
+			sd := s.makeSpanData()
+			sd.EndTime = internal.MonotonicEndTime(sd.StartTime)
+			if s.spanStore != nil {
+				s.spanStore.finished(s, sd)
 			}
-			exportersMu.Unlock()
+			if mustExport {
+				for e := range exp {
+					e.ExportSpan(sd)
+				}
+			}
 		}
 	})
 }
