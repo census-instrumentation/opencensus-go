79993219becaa7e29e3b60cb67f5b8e82dee11d6
rghetia <rghetia@yahoo.com>
2018-09-13 14:35:57 -0400

    Tracestate propagate (#901)
    * Propagate tracestate
    
    * Fixed review comments.
    
    * Fixed leading/trailing OWS removal.
    
    * Refactor to create separate package to inject/extract tracestate for http.
    
    * Fix review comments.
    
    * removed unnecessary check and import comment.
    
    * Revert "removed unnecessary check and import comment."
    
    This reverts commit 5349341a2f537cca53fda10db25fdd5260afe445.
    
    * Revert "Fix review comments."
    
    This reverts commit 40c285864c24b750071b0fdec0d150b0fdad0b85.
    
    * Revert "Refactor to create separate package to inject/extract tracestate for http."
    
    This reverts commit 5574ce881c784466edb353f6e89d0b3d83d0bbf2.
    
    * shorten variable name.

--- plugin/ochttp/propagation/tracecontext/propagation.go
+++ plugin/ochttp/propagation/tracecontext/propagation.go
@@ -24,13 +24,20 @@ import (
 
 	"go.opencensus.io/trace"
 	"go.opencensus.io/trace/propagation"
+	"go.opencensus.io/trace/tracestate"
+	"regexp"
 )
 
 const (
-	supportedVersion = 0
-	maxVersion       = 254
-	header           = "traceparent"
+	supportedVersion  = 0
+	maxVersion        = 254
+	maxTracestateLen  = 512
+	traceparentHeader = "traceparent"
+	tracestateHeader  = "tracestate"
+	trimOWSRegexFmt   = `^[\x09\x20]*(.*[^\x20\x09])[\x09\x20]*$`
 )
+
+var trimOWSRegExp = regexp.MustCompile(trimOWSRegexFmt)
 
 var _ propagation.HTTPFormat = (*HTTPFormat)(nil)
 
@@ -39,7 +46,7 @@ type HTTPFormat struct{}
 
 // SpanContextFromRequest extracts a span context from incoming requests.
 func (f *HTTPFormat) SpanContextFromRequest(req *http.Request) (sc trace.SpanContext, ok bool) {
-	h := req.Header.Get(header)
+	h := req.Header.Get(traceparentHeader)
 	if h == "" {
 		return trace.SpanContext{}, false
 	}
@@ -87,15 +94,69 @@ func (f *HTTPFormat) SpanContextFromRequest(req *http.Request) (sc trace.SpanCon
 		return trace.SpanContext{}, false
 	}
 
+	sc.Tracestate = tracestateFromRequest(req)
 	return sc, true
 }
 
-// SpanContextToRequest modifies the given request to include a header.
+// TODO(rghetia): return an empty Tracestate when parsing tracestate header encounters an error.
+// Revisit to return additional boolean value to indicate parsing error when following issues
+// are resolved.
+// https://github.com/w3c/distributed-tracing/issues/172
+// https://github.com/w3c/distributed-tracing/issues/175
+func tracestateFromRequest(req *http.Request) *tracestate.Tracestate {
+	h := req.Header.Get(tracestateHeader)
+	if h == "" {
+		return nil
+	}
+
+	var entries []tracestate.Entry
+	pairs := strings.Split(h, ",")
+	hdrLenWithoutOWS := len(pairs) - 1 // Number of commas
+	for _, pair := range pairs {
+		matches := trimOWSRegExp.FindStringSubmatch(pair)
+		if matches == nil {
+			return nil
+		}
+		pair = matches[1]
+		hdrLenWithoutOWS += len(pair)
+		if hdrLenWithoutOWS > maxTracestateLen {
+			return nil
+		}
+		kv := strings.Split(pair, "=")
+		if len(kv) != 2 {
+			return nil
+		}
+		entries = append(entries, tracestate.Entry{Key: kv[0], Value: kv[1]})
+	}
+	ts, err := tracestate.New(nil, entries...)
+	if err != nil {
+		return nil
+	}
+
+	return ts
+}
+
+func tracestateToRequest(sc trace.SpanContext, req *http.Request) {
+	var pairs = make([]string, 0, len(sc.Tracestate.Entries()))
+	if sc.Tracestate != nil {
+		for _, entry := range sc.Tracestate.Entries() {
+			pairs = append(pairs, strings.Join([]string{entry.Key, entry.Value}, "="))
+		}
+		h := strings.Join(pairs, ",")
+
+		if h != "" && len(h) <= maxTracestateLen {
+			req.Header.Set(tracestateHeader, h)
+		}
+	}
+}
+
+// SpanContextToRequest modifies the given request to include traceparent and tracestate headers.
 func (f *HTTPFormat) SpanContextToRequest(sc trace.SpanContext, req *http.Request) {
 	h := fmt.Sprintf("%x-%x-%x-%x",
 		[]byte{supportedVersion},
 		sc.TraceID[:],
 		sc.SpanID[:],
 		[]byte{byte(sc.TraceOptions)})
-	req.Header.Set(header, h)
+	req.Header.Set(traceparentHeader, h)
+	tracestateToRequest(sc, req)
 }
