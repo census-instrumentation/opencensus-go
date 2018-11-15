f983a8ef6db38ee8122a57747c09679db832c391
Ramon Nogueira <rmn@google.com>
2018-11-15 15:33:31 -0800

    Add metric API

---
+++ metric/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package metric contains a data model and exporter support for metrics.
+// The data model is derived from the metric protocol buffers:
+//   https://github.com/census-instrumentation/opencensus-proto/blob/master/src/opencensus/proto/metrics/v1/metrics.proto
+package metric // import "go.opencensus.io/metric"
