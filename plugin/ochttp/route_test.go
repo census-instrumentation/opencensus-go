3abc75f40a2357dfd55e20f1631a181c31cacb04
Márk Sági-Kazár <sagikazarmark@users.noreply.github.com>
2019-01-25 23:38:41 +0100

    Allow setting the http route from HTTP handlers (#1017)

--- plugin/ochttp/route_test.go
+++ plugin/ochttp/route_test.go
@@ -61,6 +61,43 @@ func TestWithRouteTag(t *testing.T) {
 	}
 }
 
+func TestSetRoute(t *testing.T) {
+	v := &view.View{
+		Name:        "request_total",
+		Measure:     ochttp.ServerLatency,
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{ochttp.KeyServerRoute},
+	}
+	view.Register(v)
+	var e testStatsExporter
+	view.RegisterExporter(&e)
+	defer view.UnregisterExporter(&e)
+
+	mux := http.NewServeMux()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ochttp.SetRoute(r.Context(), "/a/")
+		w.WriteHeader(204)
+	})
+	mux.Handle("/a/", handler)
+	plugin := ochttp.Handler{Handler: mux}
+	req, _ := http.NewRequest("GET", "/a/b/c", nil)
+	rr := httptest.NewRecorder()
+	plugin.ServeHTTP(rr, req)
+	if got, want := rr.Code, 204; got != want {
+		t.Fatalf("Unexpected response, got %d; want %d", got, want)
+	}
+
+	view.Unregister(v) // trigger exporting
+
+	got := e.rowsForView("request_total")
+	want := []*view.Row{
+		{Data: &view.CountData{Value: 1}, Tags: []tag.Tag{{Key: ochttp.KeyServerRoute, Value: "/a/"}}},
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("Unexpected view data exported, -got, +want: %s", diff)
+	}
+}
+
 type testStatsExporter struct {
 	vd []*view.Data
 }
