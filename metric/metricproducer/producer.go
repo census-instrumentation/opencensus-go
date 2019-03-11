1c9fa349e904ab5e280e67471b53d7c5f696ef0d
rghetia <rghetia@yahoo.com>
2019-03-11 15:51:12 -0700

    rename producer to metricproducer package. (#1053)

--- metric/metricproducer/producer.go
+++ metric/metricproducer/producer.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package producer
+package metricproducer
 
 import (
 	"go.opencensus.io/metric/metricdata"
